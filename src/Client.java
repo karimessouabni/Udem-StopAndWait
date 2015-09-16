4441604b043542348fd1072c2a55e532f1dcdf4a
karim <karim@mbp-de-karim.zyxel.com>
2015-09-15 22:59:52 -0400

    maj

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -8,13 +11,22 @@ public class Client {
 	
 	public static void main(String[] zero) {
 		
+		
 		Socket socket;
+		BufferedReader in;
+		PrintWriter out;
 
 		try {
 		
-		     socket = new Socket(InetAddress.getLocalHost(),2009);	
-	             socket.close();
+			socket = new Socket(InetAddress.getLocalHost(),2009);	
+		        System.out.println("Demande de connexion");
 
+		        in = new BufferedReader (new InputStreamReader (socket.getInputStream()));
+		        String message_distant = in.readLine();
+		        System.out.println(message_distant);
+		        
+		        socket.close();
+		       
 		}catch (UnknownHostException e) {
 			
 			e.printStackTrace();
@@ -25,3 +37,4 @@ public class Client {
 	}
 
 }
+
--- src/Server.java
+++ src/Server.java
@@ -1,25 +1,39 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.io.PrintWriter;
 
 public class Server {
-
+	
 	public static void main(String[] zero) {
 		
 		ServerSocket socketserver  ;
 		Socket socketduserveur ;
-
+		BufferedReader in;
+		PrintWriter out;
+		
 		try {
 		
 			socketserver = new ServerSocket(2009);
+			System.out.println("Le serveur est √† l'√©coute du port "+socketserver.getLocalPort());
 			socketduserveur = socketserver.accept(); 
-			System.out.println("Un zéro s'est connecté !");
-		        socketserver.close();
+			out = new PrintWriter(socketduserveur.getOutputStream());
+		        out.println("Connexion reussie. Bienvenue !");
+		        out.flush();
+		                
 		        socketduserveur.close();
-
+		        socketserver.close();
+		        
 		}catch (IOException e) {
+			
 			e.printStackTrace();
 		}
 	}
 
 }
+
